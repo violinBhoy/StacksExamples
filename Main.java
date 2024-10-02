6f7e01c9b421de4fc48c113863aab290cc53a722
Andrew Sautel <152945937+violinBhoy@users.noreply.github.com>
2024-10-02 17:20:42 +0000

    created my own stack class and main that uses it

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String args[]) {
+        Stack<String> stack = new Stack<>();
+        stack.push("A");
+        System.out.println(stack.peek());
+    }
+}
---
+++ Stack.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Stack<T> {
+    private ArrayList<T> stack;
+
+    public Stack() {
+        stack = new ArrayList<>();
+    }
+
+    public void push(T item) {
+        stack.add(item);
+    }
+
+    public T pop() {
+        if (isEmpty())
+            return null;
+        return stack.remove(stack.size()-1);
+    }
+
+    public T peek() {
+        if (isEmpty())
+            return null;
+        return stack.get(stack.size()-1);
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+
+    public int size() {
+        return stack.size();
+    }
+}
